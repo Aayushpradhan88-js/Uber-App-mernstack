718f140e785541230f177fe7505703d06e46fb78
Ayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2025-02-14 20:16:39 +0545

    writing user signup code

--- Frontend/vite-project/src/pages/UserLogin.jsx
+++ Frontend/vite-project/src/pages/UserLogin.jsx
@@ -1,32 +1,86 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const UserLogin = () => {
+
+  // twoway binding 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userData, setUserData] = useState({});
+
+  function submitHandler(e) {
+    e.preventDefault(); //To stop the output of the browser
+    setUserData({
+      email: email,
+      password: password
+    })
+    setEmail('')
+    setPassword('')
+  }
+
   return (
-    <div className='p-7'>
-      <img className='w-16 mb-10' src="https://www.logo.wine/a/logo/Uber/Uber-Logo.wine.svg" alt="" />
-      <form>
-        <h3 className='text-xl font-medium mb-2'>What is Your Email</h3>
-        <input
-          required
-          className='bg-[#eeeeee] mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'
-          type="email"
-          placeholder='email@example.com'
-        />
+    <div className='p-7 h-screen flex flex-col justify-between'>
+      <div>
+        {/* uber logo */}
+        <img className='w-16 mb-10' src="https://th.bing.com/th/id/OIP.9bm6M6gfXxYJ6qqjNR9B9AAAAA?w=280&h=280&rs=1&pid=ImgDetMain" alt="" />
 
-        <h3 className='text-xl font-medium mb-2'>Enter Password</h3>
+        {/* Form  */}
+        <form onSubmit={(e) => {
+          submitHandler(e);
+        }}>
+          {/* email box */}
+          <h3 className='text-xl font-medium mb-2'>
+            What is Your Email
+          </h3>
 
-        <input
-          required type="password"
-          className='bg-[#eeeeee] mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'
-          placeholder='password'
-        />
+          <input
+            required
+            // twoway binding 
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            className='bg-[#eeeeee] mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'
+            type="email"
+            placeholder='email@example.com'
+          />
 
-        <button className='bg-[#eeeeee] font-semibold mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'>Login</button>
-      </form>
+          {/* password box */}
+          <h3 className='text-xl font-medium mb-2'>
+            Enter Password
+          </h3>
 
-      {/* <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p> */}
+          <input
+            required
+            type="password"
+            // twoway binding 
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            className='bg-[#eeeeee] mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'
+            placeholder='password'
+          />
+
+          {/* Login button */}
+          <button className='bg-[#eeeeee] font-semibold mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'>
+            Login
+          </button>
+
+        </form>
+
+        {/* redirected to new Account */}
+        <p className='text-center'>New here?
+          <Link to='/signup' className='text-blue-600'>
+            Create new Account
+          </Link>
+        </p>
+      </div>
+
+      <div>
+        <button className='bg-green-500 font-semibold mb-7 px-4 py-2 border w-full text-lg placeholder:text rounded-lg'>Sign in as Captain</button>
+      </div>
     </div>
   )
 }
